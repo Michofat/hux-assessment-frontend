a924a5a2df4f501e3c5810fa3efedbe076b7adff
Michofat <michofatltd@gmail.com>
2024-05-07 09:22:10 +0100

    fixed bug in adding new contact

--- src/pages/NewContact.jsx
+++ src/pages/NewContact.jsx
@@ -1,91 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import SideBar from "../components/SideBar";
 import axios from "axios";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { Modal, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const Dashboard = () => {
-  const [contacts, setContacts] = useState([]);
-  const [selectedContact, setSelectedContact] = useState(null);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [editedContact, setEditedContact] = useState(selectedContact || {});
+const NewContact = () => {
+  const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  useEffect(() => {
-    fetchContacts();
-  }, []);
-
-  const fetchContacts = async () => {
+  const onSubmit = async (data) => {
     try {
-      const response = await axios.get("http://localhost:8000/api/v1/contacts");
-      const result = response.data.result;
-      setContacts(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleEdit = (contact) => {
-    setSelectedContact(contact);
-    setEditedContact(contact); // Set editedContact state to the selected contact
-    setIsEditModalOpen(true);
-  };
-
-  const handleUpdate = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.patch(
-        `http://localhost:8000/api/v1/contact/${selectedContact.contactId}`,
-        {
-          firstName: selectedContact.firstName,
-          lastName: selectedContact.lastName,
-          phoneNumber: selectedContact.phoneNumber,
-        }
+      setIsLoading(true);
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/contact",
+        data
       );
-      closeModal();
-      fetchContacts(); // Assuming fetchContacts function fetches the updated contact list
-      alert("Contact updated successfully");
+      alert("Contact submitted successfully:", response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.error("Error updating contact:", error);
-      alert("Failed to update contact");
+      alert("Failed to submit contact:", error);
+      setIsLoading(false);
     }
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditedContact({
-      ...editedContact,
-      [name]: value,
-    });
-    console.log("setEditedContact", setEditedContact);
-  };
-
-  const handleDelete = (contact) => {
-    setSelectedContact(contact);
-    setIsDeleteModalOpen(true);
-  };
-
-  const confirmDelete = async () => {
-    try {
-      await axios.delete(
-        `http://localhost:8000/api/v1/contact/${selectedContact.contactId}`
-      );
-      closeModal();
-      fetchContacts(); // Assuming fetchContacts function fetches the updated contact list
-      alert("Contact deleted successfully");
-    } catch (error) {
-      console.error("Error deleting contact:", error);
-      alert("Failed to delete contact");
-    }
-  };
-
-  const closeModal = () => {
-    setSelectedContact(null);
-    setIsEditModalOpen(false);
-    setIsDeleteModalOpen(false);
-    fetchContacts();
   };
 
   return (
@@ -102,161 +43,79 @@ const Dashboard = () => {
       <div className="flex">
         <SideBar />
 
-        {/* Main Page */}
-        <main className="w-10/12 p-4">
-          <h2 className="text-2xl font-semibold mb-4">All Contacts</h2>
-          <table className="w-full border-collapse border border-gray-300">
-            <thead className="bg-gray-200">
-              <tr>
-                <th className="py-2 px-4 border border-gray-300 text-left">
-                  First Name
-                </th>
-                <th className="py-2 px-4 border border-gray-300 text-left">
-                  Last Name
-                </th>
-                <th className="py-2 px-4 border border-gray-300 text-left">
-                  Phone Number
-                </th>
-                <th className="py-2 px-4 border border-gray-300 text-left">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {contacts.map((contact, index) => (
-                <tr key={index}>
-                  <td className="py-2 px-4 border border-gray-300">
-                    {contact.firstName}
-                  </td>
-                  <td className="py-2 px-4 border border-gray-300">
-                    {contact.lastName}
-                  </td>
-                  <td className="py-2 px-4 border border-gray-300">
-                    {contact.phoneNumber}
-                  </td>
-                  <td className="py-2 px-4 border border-gray-300">
-                    <button
-                      className="mr-2"
-                      onClick={() => handleEdit(contact)}
-                    >
-                      <EditIcon />
-                    </button>
-                    <button onClick={() => handleDelete(contact)}>
-                      <DeleteIcon />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </main>
-      </div>
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-lg text-center">
+            <h1 className="text-2xl font-bold sm:text-3xl">Contact Form</h1>
+            <p className="mt-4 text-gray-500">
+              Please enter the details below to submit the contact
+            </p>
+          </div>
 
-      {/* Edit Modal */}
-      <Modal
-        open={isEditModalOpen}
-        onClose={closeModal}
-        aria-labelledby="edit-modal-title"
-        className="flex justify-center items-center"
-      >
-        {/* Modal Content */}
-        <div className="modal-content bg-white p-8 rounded-lg shadow-lg w-96">
-          <h2 id="edit-modal-title" className="text-xl font-bold mb-4">
-            Edit Contact
-          </h2>
-          <form onSubmit={handleUpdate}>
-            {/* First Name */}
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="firstName" className="mb-1">
-                First Name:
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mb-0 mt-8 max-w-md space-y-4"
+          >
+            <div>
+              <label htmlFor="firstName" className="sr-only">
+                First Name
               </label>
               <input
                 type="text"
                 id="firstName"
-                name="firstName"
-                value={editedContact.firstName || ""}
-                onChange={handleInputChange}
-                className="rounded-lg border border-gray-300 px-3 py-2 mb-2"
+                {...register("firstName", { required: true })}
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter First Name"
               />
+              {errors.firstName && (
+                <p className="text-sm text-red-500">First Name is required</p>
+              )}
             </div>
-            {/* Last Name */}
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="lastName" className="mb-1">
-                Last Name:
+
+            <div>
+              <label htmlFor="lastName" className="sr-only">
+                Last Name
               </label>
               <input
                 type="text"
                 id="lastName"
-                name="lastName"
-                value={editedContact.lastName || ""}
-                onChange={handleInputChange}
-                className="rounded-lg border border-gray-300 px-3 py-2 mb-2"
+                {...register("lastName", { required: true })}
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter Last Name"
               />
+              {errors.lastName && (
+                <p className="text-sm text-red-500">Last Name is required</p>
+              )}
             </div>
-            {/* Phone Number */}
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="phoneNumber" className="mb-1">
-                Phone Number:
+
+            <div>
+              <label htmlFor="phoneNumber" className="sr-only">
+                Phone Number
               </label>
               <input
                 type="text"
                 id="phoneNumber"
-                name="phoneNumber"
-                value={editedContact.phoneNumber || ""}
-                onChange={handleInputChange}
-                className="rounded-lg border border-gray-300 px-3 py-2 mb-2"
+                {...register("phoneNumber", { required: true })}
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter Phone Number"
               />
+              {errors.phoneNumber && (
+                <p className="text-sm text-red-500">Phone Number is required</p>
+              )}
             </div>
-            {/* Update Button */}
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2"
-              >
-                Update
-              </button>
-              {/* Cancel Button */}
-              <button
-                type="button"
-                onClick={closeModal}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg"
-              >
-                Cancel
-              </button>
-            </div>
+
+            <button
+              type="submit"
+              className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+            >
+              {isLoading ? (
+                <CircularProgress size={24} color="inherit" />
+              ) : (
+                "Submit"
+              )}
+            </button>
           </form>
         </div>
-      </Modal>
-      {/* Delete Modal */}
-      {/* Delete Modal */}
-      <Modal
-        open={isDeleteModalOpen}
-        onClose={closeModal}
-        aria-labelledby="delete-modal-title"
-      >
-        <div className="modal-content p-8 rounded-lg shadow-lg bg-white z-50">
-          <h2 id="delete-modal-title" className="text-xl font-bold mb-4">
-            Delete Contact
-          </h2>
-          <p>Are you sure to delete {selectedContact?.firstName}?</p>
-          {/* Delete Button */}
-          <div className="flex justify-end mt-4">
-            <button
-              onClick={confirmDelete}
-              className="bg-red-500 text-white px-4 py-2 rounded-lg"
-            >
-              Delete
-            </button>
-            {/* Cancel Button */}
-            <button
-              onClick={closeModal}
-              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg ml-4"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </Modal>
+      </div>
     </div>
   );
 };
@@ -266,4 +125,4 @@ function logout() {
   alert("Logout button clicked");
 }
 
-export default Dashboard;
+export default NewContact;
